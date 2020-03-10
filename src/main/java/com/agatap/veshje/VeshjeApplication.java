4e9e62ca34f50c97d25b0c69050a3d3c8e3999b3
apytka <56727542+apytka@users.noreply.github.com>
2020-03-10 13:07:33 +0100

    first commit

---
+++ src/main/java/com/agatap/veshje/VeshjeApplication.java
@@ -0,0 +1,13 @@
+package com.agatap.veshje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeshjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeshjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agatap/veshje/veshje/VeshjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agatap.veshje.veshje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeshjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
