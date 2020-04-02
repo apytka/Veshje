092c001cf4ef8f1e4680e9d7699e266ee73af888
apytka <56727542+apytka@users.noreply.github.com>
2020-04-02 13:01:09 +0200

    add account page with security

--- src/main/java/com/agatap/veshje/view/UsersViewController.java
+++ src/main/java/com/agatap/veshje/view/UsersViewController.java
@@ -1,9 +1,13 @@
 package com.agatap.veshje.view;
 
+import com.agatap.veshje.controller.DTO.CreateUpdateNewsletterDTO;
 import com.agatap.veshje.controller.DTO.CreateUserDTO;
+import com.agatap.veshje.model.User;
+import com.agatap.veshje.service.NewsletterService;
 import com.agatap.veshje.service.UserService;
 import com.agatap.veshje.service.exception.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,15 +21,10 @@ import javax.validation.Valid;
 public class UsersViewController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private NewsletterService newsletterService;
 
-    @GetMapping("/login")
-    public ModelAndView displayLoginAndRegister() {
-        ModelAndView modelAndView = new ModelAndView("login");
-        modelAndView.addObject("createUser", new CreateUserDTO());
-        return modelAndView;
-    }
-
-    @PostMapping("/login")
+    @PostMapping("/register")
     public ModelAndView createUser(@Valid @ModelAttribute(name="createUser") CreateUserDTO createUserDTO, BindingResult bindingResult)
             throws UserDataInvalidException, UserAlreadyExistException, AddressDataInvalidException {
         if(bindingResult.hasErrors()) {
@@ -33,5 +32,27 @@ public class UsersViewController {
         }
         userService.createUser(createUserDTO);
         return new ModelAndView("redirect:index");
+    }
+
+    @GetMapping("/account")
+    public ModelAndView displayAccountUser(@ModelAttribute(name = "addNewsletterAccount")
+            CreateUpdateNewsletterDTO createUpdateNewsletterDTO, Authentication authentication)
+            throws UserNotFoundException {
+        User user = userService.findUserByEmail(authentication.getName());
+        ModelAndView modelAndView = new ModelAndView("account");
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("createAccountDto", new CreateUpdateNewsletterDTO());
+        return modelAndView;
+    }
+
+    @PostMapping("/account")
+    public ModelAndView addNewsletterInAccount(
+            @Valid @ModelAttribute(name = "addNewsletterAccount") CreateUpdateNewsletterDTO createUpdateNewsletterDTO, BindingResult bindingResult)
+            throws NewsletterAlreadyExistsException, NewsletterDataInvalidException {
+        if(bindingResult.hasErrors()) {
+            return new ModelAndView("account");
+        }
+        newsletterService.createNewsletterDTO(createUpdateNewsletterDTO);
+        return new ModelAndView("redirect:account");
     }
 }
--- src/main/java/com/agatap/veshje/view/ViewController.java
+++ src/main/java/com/agatap/veshje/view/ViewController.java
@@ -9,15 +9,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ViewController {
 
-//    @GetMapping("/login")
-//    public String login() {
-//        return "login";
-//    }
-
     @GetMapping({"", "/", "/index"})
     public ModelAndView displayMainSite() {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("addNewsletter", new CreateUpdateNewsletterDTO());
         return modelAndView;
     }
+
+    @GetMapping("/login")
+    public ModelAndView displayLoginAndRegister() {
+        ModelAndView modelAndView = new ModelAndView("login");
+        modelAndView.addObject("createUser", new CreateUserDTO());
+        return modelAndView;
+    }
+
+
+
+
 }
