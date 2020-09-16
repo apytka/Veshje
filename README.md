6d41981c78b8d287ebd2e873287793f8b4de4503
apytka <56727542+apytka@users.noreply.github.com>
2020-09-16 12:45:50 +0200

    add README.md file and correct account-orders-details (wrong search path)

---
+++ README.md
@@ -0,0 +1,9 @@
+README.md
+
+![Veshje](src/main/resources/static/img/github/veshje.png)
+
+
+
+# Veshje - online shop
+
+The expected date of adding to server - September 2020
--- src/main/java/com/agatap/veshje/VeshjeApplication.java
+++ src/main/java/com/agatap/veshje/VeshjeApplication.java
@@ -2,11 +2,12 @@ package com.agatap.veshje;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public class VeshjeApplication {
+public class VeshjeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeshjeApplication.class, args);
